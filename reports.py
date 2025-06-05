5445cb1f0666ecef9b7d759e1ae6c7eb06d82c32
Mark Parquette <markrparquette@gmail.com>
2025-06-05 13:24:04 -0400

    master: Add average mean and max to reports for total and discounted rewards.

--- reports.py
+++ reports.py
@@ -20,7 +20,7 @@ def gen_detailed_report(
     seed=range(10),
     path='./baseline_results'
 ):
-    print("Seed,                       Env,     Policy,     Ave,     Max,    (Ave),    (Max),  Count")
+    print("Seed,                       Env,     Policy,    Mean,     Max,     Ave,     Max,   (Mean),    (Max),    (Ave),    (Max),  Count")
 
     for e in envs:
         for s in seed:
@@ -31,12 +31,18 @@ def gen_detailed_report(
                 if data is None or disc_data is None:
                     continue
 
-                ave = sliding_average(data, 10)
-                ave_mean = np.mean(ave)
-                ave_max = np.max(data)
+                ud_mean = np.mean(data)
+                ud_max = np.max(data)
 
-                disc_ave = sliding_average(disc_data, 10)
-                disc_ave_mean = np.mean(disc_ave)
-                disc_ave_max = np.max(disc_data)
+                ud_sa = sliding_average(data, 10)
+                ave_mean = np.mean(ud_sa)
+                ave_max = np.max(ud_sa)
+
+                disc_mean = np.mean(disc_data)
+                disc_max = np.max(disc_data)
+
+                d_sa = sliding_average(disc_data, 10)
+                disc_ave_mean = np.mean(d_sa)
+                disc_ave_max = np.max(d_sa)
                 
-                print(f"{s:>4}, {e:>25}, {p:>10}, {ave_mean:>7.2f}, {ave_max:>7.2f}, {disc_ave_mean:>8.2f}, {disc_ave_max:>8.2f}, {len(data):>6}")
+                print(f"{s:>4}, {e:>25}, {p:>10}, {ud_mean:>7.2f}, {ud_max:>7.2f}, {ave_mean:>7.2f}, {ave_max:>7.2f}, {disc_mean:>8.2f}, {disc_max:>8.2f}, {disc_mean:>8.2f}, {disc_max:>8.2f}, {len(data):>6}")
